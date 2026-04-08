e109ead75ff8330ab52b4a365335ccc4850c1837
seva <sevakogan@gmail.com>
2026-04-08 00:45:37 -0700

    chore: remove basePath for subdomain deployment

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  basePath: "/beta.v2",
-  images: {
-    unoptimized: false,
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
